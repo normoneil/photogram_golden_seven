488ba301ba6d30490ff9a9b9dd333f60469fe96c
normoneil <normoneil@users.noreply.github.com>
2016-10-30 22:05:09 -0500

    7 failures, 7/14 points, 50.0%

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,14 +4,6 @@ class PhotosController < ApplicationController
     render("photos/index.html.erb")
   end
 
-  def new_form
-    render("photos/new.html.erb")
-  end
-
-  def create_row
-    render("photos/create.html.erb")
-  end
-
   def show
     @id = params["id"]
     @source = Photo.find(@id).source
@@ -20,5 +12,28 @@ class PhotosController < ApplicationController
     render("photos/show.html.erb")
   end
 
+  def new_form
+    render("photos/new.html.erb")
+  end
+
+  def create_row
+    @caption = params["the_caption"].to_s
+    @source = params["the_source"].to_s
+    create = Photo.new
+    create.caption = @caption
+    create.source = @source
+    create.save
+    redirect_to("http://localhost:3000/photos")
+  end
+
+
+
+  def show
+    @id = params["id"]
+    @source = Photo.find(@id).source
+    @caption = Photo.find(@id).caption
+    # @caption = Photo.find({ :id => @id }).caption
+    render("photos/show.html.erb")
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   get("/", { :controller => "photos", :action => "index" })
 
   # Routes to READ photos
-  get("/photos",       { :controller => "photos", :action => "index" })
-  get("/photos/new",   { :controller => "photos", :action => "new_form" })
-  get("/create_photo", { :controller => "photos", :action => "create_row" })
-  get("/photos/:id",   { :controller => "photos", :action => "show" })
+  get("/photos",           { :controller => "photos", :action => "index" })
+  get("/photos/new",       { :controller => "photos", :action => "new_form" })
+  get("/create_photo",     { :controller => "photos", :action => "create_row" })
+  get("/delete_photo/:id", { :controller => "photos", :action => "destroy" })
+  get("/photos/:id",       { :controller => "photos", :action => "show" })
 
 end
 #
