3971db57d942256651280ca39339f71b1947f55e
normoneil <normoneil@users.noreply.github.com>
2016-10-30 21:37:47 -0500

    8 failures, 6/14 points, 42.857142857142854%

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,23 @@
 class PhotosController < ApplicationController
   def index
     @list_of_photos = Photo.all
+    render("photos/index.html.erb")
+  end
+
+  def new_form
+    render("photos/new.html.erb")
+  end
+
+  def create_row
+    render("photos/create.html.erb")
+  end
+
+  def show
+    @id = params["id"]
+    @source = Photo.find(@id).source
+    @caption = Photo.find(@id).caption
+    # @caption = Photo.find({ :id => @id }).caption
+    render("photos/show.html.erb")
   end
 
 
